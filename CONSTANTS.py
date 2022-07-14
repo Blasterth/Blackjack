66f09952339dfb1b87e4ef83d6d9c08629ad3943
Parsa Aryan <93974116+Blasterth@users.noreply.github.com>
2022-07-14 20:38:06 +0430

    New (files including:) variables and a Class

---
+++ CONSTANTS.py
@@ -0,0 +1,6 @@
+SYMBOL_LIST = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
+SUIT_LIST = ["Hearts","Spades","Diamonds","Clubs"]
+FACE_CARDS_LIST = SYMBOL_LIST[10:14]
+EMPTY_INPUT_ERROR_MESSAGE = "No input entered!"
+BAD_INPUT_ERROR_MESSAGE = "Command not recognized!"
+OUT_OF_RANGE_ERROR_MESSAGE = "Out of range!"
